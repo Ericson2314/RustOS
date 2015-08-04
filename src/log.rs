82b30d157a0ecefe6e196ca34c3ef421b486de0f
John Ericson <Ericson2314@Yahoo.com>
2015-08-04 14:44:37 -0700

    Fix race and potential deadlock with printing/logging

--- src/log.rs
+++ src/log.rs
@@ -1,18 +1,22 @@
+use core::fmt::Arguments;
+use io::Writer;
+
+pub fn write(args: Arguments) {
+  // Arguments are already evaluated, so dead-lock is avoided
+  ::terminal::GLOBAL.lock().write_fmt(args).ok();
+}
+
 //#[macro_export]
 macro_rules! __print(
-  ($($arg:tt)*) => ({
-    use io::Writer;
-    write!(::terminal::GLOBAL.lock(), $($arg)*).ok();
-  })
+  ($($arg:tt)*) => (::log::write(format_args!($($arg)*)))
 );
 
 #[macro_export]
 macro_rules! log(
-  ($lvl: expr, $($arg:tt)*) => ({
-    __print!("[{}:{} {}]: ", $lvl, file!(), line!());
-    __print!($($arg)*);
-    __print!("\n");
-  })
+  ($lvl: expr, $($arg:tt)*) => (
+    // Must be one print for atomicity
+    __print!("[{}:{} {}]: {}\n", $lvl, file!(), line!(), format_args!($($arg)*))
+  )
 );
 
 #[macro_export]
