f387c7fe3b75cd3f5a8b39143c0933911d0c8e5e
John Ericson <Ericson2314@Yahoo.com>
2016-07-13 22:48:35 -0700

    Update Rust

--- default.nix
+++ default.nix
@@ -14,10 +14,10 @@ let
   funs = callPackage "${settings}/user/.nixpkgs/rust-nightly.nix" { };
 
   cargoNightly = funs.cargo {
-    date = "2016-07-07";
+    date = "2016-07-12";
   };
 
-  rustDate = "2016-07-07";
+  rustDate = "2016-07-12";
 
   rustcNightly = funs.rustc {
     date = rustDate;
